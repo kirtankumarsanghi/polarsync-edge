8166518771fe87178024eb9740b1b23aaee989bf
Kirtan Sanghi <kirtankumarsanghi@gmail.com>
2026-09-08 23:47:08 +0530

    Remove unnecessary files

--- frontend/fix.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs'); 
-const file = 'src/app/dashboard/page.tsx'; 
-let data = fs.readFileSync(file, 'utf8'); 
-data = data.replace(/\\`/g, '`').replace(/\\\$\{/g, '${'); 
-fs.writeFileSync(file, data);
